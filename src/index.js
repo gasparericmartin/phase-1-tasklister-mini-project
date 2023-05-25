a440f3e6099add91c130165f6fde4193e97618aa
Eric-Martin <gaspar.ericmartin@gmail.com>
2023-05-25 18:25:19 -0400

    Complete tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  
+  document.getElementById('create-task-form').addEventListener('submit', function (e) {
+    submitter(e)
+  })
+  
+ })
+
+
+function submitter(e) {
+  e.preventDefault()
+  const newText = document.getElementById('new-task-description').value
+  if (newText) {
+    const textInsert = document.createElement('p')
+    const button = document.createElement('button')
+    button.addEventListener('click', function (e) {
+      e.target.parentNode.remove()
+    })
+    button.textContent = 'x'
+    textInsert.textContent = newText
+  
+    textInsert.appendChild(button)
+
+  
+  
+    document.getElementById('tasks').appendChild(textInsert)
+  }
+  
+}
+
+
+
+
+
